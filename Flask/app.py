c34a1cbd91baf3ec9aa7a522cf08e72d892e632b
TKR <ksw2270273@stu.o-hara.ac.jp>
2024-01-16 11:48:48 +0900

    seo

--- Flask/app.py
+++ Flask/app.py
@@ -4,13 +4,10 @@ from index import index_bp
 from docker_mysql import docker_mysql_bp
 from register import register_bp
 from signup import signup_bp
-<<<<<<< HEAD
 from eventlist import eventlist_bp
-=======
 from chat import chat_bp
 from creategroup import creategroup_bp
 from userlist import userlist_bp
->>>>>>> 308d533963e147f82d723fee6c4bd4e3abaf121b
 
 app = Flask(__name__)
 app.register_blueprint(testIndex_bp)
@@ -18,13 +15,10 @@ app.register_blueprint(index_bp)
 app.register_blueprint(docker_mysql_bp)
 app.register_blueprint(register_bp)
 app.register_blueprint(signup_bp)
-<<<<<<< HEAD
 app.register_blueprint(eventlist_bp)
-=======
 app.register_blueprint(chat_bp)
 app.register_blueprint(creategroup_bp)
 app.register_blueprint(userlist_bp)
->>>>>>> 308d533963e147f82d723fee6c4bd4e3abaf121b
 
 @app.route("/")
 def show_urls():
