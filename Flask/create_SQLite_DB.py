0c6a79084b79653360a9d5f01478079024775120
somePC <ksw2270227@stu.o-hara.ac.jp>
2024-01-17 12:06:49 +0900

    染谷 SQLiteに対応

--- Flask/create_SQLite_DB.py
+++ Flask/create_SQLite_DB.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template, jsonify, Blueprint
-import mysql.connector
+import os
+print("Current Directory:", os.getcwd())
+
+from flask import Flask, render_template, Blueprint
 import sqlite3
-check_data_bp = Blueprint('check_data', __name__)
+
+create_SQLite_DB_bp = Blueprint('create_SQLite_DB', __name__)
+
+def reset_db():
+    db_path = 'testDB.db'
+    if os.path.exists(db_path):
+        os.remove(db_path)
+
+    # データベースファイルが存在しない場合、新しいデータベースが自動的に作成される
+    conn = sqlite3.connect(db_path)
+    return conn
 
 def get_db_connection():
     conn = sqlite3.connect('testDB.db')
     return conn
 
+def create_db_from_sql(sql_file_path):
+    conn = reset_db()  # 新しいデータベースファイルを作成
+    cur = conn.cursor()
+
+    with open(sql_file_path, 'r', encoding='utf-8') as sql_file:
+        sql_script = sql_file.read()
+
+    cur.executescript(sql_script)
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@create_SQLite_DB_bp.route('/create_SQLite_DB')
 def show_tables():
+    create_db_from_sql('Flask/init.sql')  # 最初にデータベースをリセットして初期化
+
     conn = get_db_connection()  # リセット後のデータベースに接続
     cur = conn.cursor()
 
@@ -50,9 +77,6 @@ def show_tables():
     cur.close()
     conn.close()
 
-    return render_template('check_data.html', users=users,admins=admins ,companies=companies,companies_employee=companies_employee,groups=groups,events=events,messages=messages,location_data=location_data,location_history=location_history)
+    return render_template('create_SQLite_DB.html', users=users,admins=admins ,companies=companies,companies_employee=companies_employee,groups=groups,events=events,messages=messages,location_data=location_data,location_history=location_history)
 
 
-@check_data_bp.route('/check_data')
-def check_data():
-    return show_tables()
