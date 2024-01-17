cefbd33d2704d9bfa67c7aefb85e43b73f98d8a5
Saitou <ksw2270119@stu.o-hara.ac.jp>
2024-01-17 12:13:31 +0900

    sa

--- Flask/chat.py
+++ Flask/chat.py
@@ -1,42 +1,17 @@
 from flask import Flask, render_template, request
 import mysql.connector
+import sqlite3
 from flask import Blueprint
 
 chat_bp = Blueprint('chat', __name__)
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host='localhost',
-        database='testDB1',
-        user='root',
-        password='pass'
-    )
-
-def create_messages_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
-    # messagesテーブルが存在しない場合に作成するクエリ
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS messages (
-        message_id INT AUTO_INCREMENT PRIMARY KEY,
-        sender_user_id VARCHAR(10),
-        receiver_user_id VARCHAR(10),
-        message_content TEXT,
-        sent_time DATETIME
-    )
-    """
-
-    cursor.execute(create_table_query)
-    conn.commit()
-
-    cursor.close()
-    conn.close()
+    conn = sqlite3.connect('testDB.db')
+    return conn
 
 
 @chat_bp.route('/chat', methods=['GET', 'POST'])
 def chat():
-    create_messages_table()  # アプリケーション起動時にテーブルを作成
     if request.method == 'POST':
         message_content = request.form['message']
         save_message(message_content)
