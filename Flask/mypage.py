b7677fa5312cbda5c9dcac068a2c5bbaa7ff9ab5
TKR <ksw2270273@stu.o-hara.ac.jp>
2024-01-18 12:10:19 +0900

    瀬尾

--- Flask/mypage.py
+++ Flask/mypage.py
@@ -15,22 +15,36 @@ def mypage():
 
     if not user_id:
         # ユーザーがログインしていない場合はログインページにリダイレクト
-        return redirect(url_for('login.login_user'))
+        print("ログインしてない")
+        return redirect(url_for('index.index'))
 
     conn = get_db_connection()
     cursor = conn.cursor()
 
-    # ユーザーIDを使用してユーザーの情報を取得
-    cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
-    user_info = cursor.fetchone()
+    try:
+        # ユーザーIDを使用してユーザーの情報を取得
+        cursor.execute('SELECT * FROM users WHERE user_id = ?', (user_id,))
+        user_info = cursor.fetchone()
 
-    cursor.close()
-    conn.close()
+        if user_info:
+            # マイページにユーザー情報を渡して表示
+            print("たから")
+            print(user_info)
+            return render_template('mypage.html', user_info=user_info)
+        else:
+            # ユーザー情報が見つからない場合はエラーを表示
+            return render_template('error.html', message='ユーザー情報が見つかりません。')
 
-    if user_info:
-        # マイページにユーザー情報を渡して表示。
-        return render_template('mypage.html', user_info=user_info)
-    else:
-        # ユーザー情報が見つからない場合はエラーを表示
-        return render_template('error.html', message='ユーザー情報が見つかりません。')
-    
+    except Exception as e:
+        # エラーが発生した場合はエラーページを表示
+        # return render_template('error.html', message=str(e))
+        print(e)
+        print("なんかエラーでた")
+        return redirect(url_for('index.index'))
+
+    finally:
+        cursor.close()
+        conn.close()
+
+
+
