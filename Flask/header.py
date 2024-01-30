12e01a3520996c11d654a3e2f9a2ea419c6a3e6b
Saitou <ksw2270119@stu.o-hara.ac.jp>
2024-01-30 15:13:38 +0900

    sa header

--- Flask/header.py
+++ Flask/header.py
@@ -11,41 +11,64 @@ group_bp = Blueprint('group', __name__)
 map_bp = Blueprint('map', __name__)
 logout_bp = Blueprint('logout', __name__)
 
-# ルート設定
-@index_bp.route('/index')
-def index():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+header_bp = Blueprint('header', __name__)
 
-# 未ログイン
-@signup_bp.route('/signup')
-def signup():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+# # ルート設定
+# @index_bp.route('/index', methods=['GET', 'POST'])
+# def index():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
 
-@login_bp.route('/login')
-def login():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+# # 未ログイン
+# @signup_bp.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
+
+# @login_bp.route('/login', methods=['GET', 'POST'])
+# def login():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
+
+
 
 
 # ログイン済み
-@mypage_bp.route('/mypage')
-def chat():
-    user_id = session.get('user_id')
+@header_bp.route('*', methods=['GET', 'POST'])
+def mypage():
+    user_id = int(session.get('user_id'))
+    print(f"user_id: {user_id}")
     return render_template('header.html', user_id=user_id)
 
-@chat_bp.route('/chat')
-def chat():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+# @chat_bp.route('/chat', methods=['GET', 'POST'])
+# def chat():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
 
-@map_bp.route('/map')
-def map():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+# @map_bp.route('/map', methods=['GET', 'POST'])
+# def map():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
 
-@logout_bp.route('/logout')
-def logout():
-    user_id = session.get('user_id')
-    return render_template('header.html', user_id=user_id)
+# @login_bp.route('/group', methods=['GET', 'POST'])
+# def group():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
+
+# @login_bp.route('/show_event', methods=['GET', 'POST'])
+# def event():
+#     user_id = int(session.get('user_id'))
+#     print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
+
+# @logout_bp.route('/logout', methods=['GET', 'POST'])
+# def logout():
+#     user_id = session.get('user_id')
+        # print(f"user_id: {user_id}")
+#     return render_template('header.html', user_id=user_id)
